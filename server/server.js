a54abe1e93647bf8af90adf52b7a956eefca2d52
Himanshu Prajapati <hp807655@gmail.com>
2026-09-13 15:47:19 +0530

    chore: increase auth rate limit to 50 req/15min

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ app.use(sanitizeMiddleware);
 
 const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 20,
+  max: 50,
   standardHeaders: true,
   legacyHeaders: false,
   message: { success: false, message: 'Too many attempts, please try again later.' },
